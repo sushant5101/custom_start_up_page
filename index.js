b40763fb76f703babe2fdf104e2fc654a153876c
sushant <aarosh27@gmail.com>
2025-01-14 07:43:45 +0545

    refactoring the javascript

---
+++ index.js
@@ -0,0 +1,156 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const radiobutton = document.querySelectorAll('input[name=engine]')
+    const user_question = document.getElementById("searchbox")
+    const formdetails = document.getElementById("searchcontainer")
+    const shortcutscontainer = document.getElementById("shortcuts")
+    const popupbox = document.getElementById("popupbox")
+    const popupwindow = document.getElementById("popupwindow")
+    const logo = document.getElementById("logo")
+    const shortname = document.getElementById("name")
+    const url = document.getElementById("url")
+    const shortlabel = document.getElementById("shortlabel")
+    const changebg = document.getElementById("changebg")
+    const auto = document.getElementById("auto")
+    const settingicon = document.getElementById("settingicon")
+    const container = document.getElementById("container")
+    const controllbox = document.getElementById("controll")
+    const changeduration = document.getElementById("duration")
+    const bgimg = document.getElementById("bgimg")
+    const imgpreview = document.getElementById("imgpreview")
+    const resultpopupbox = document.getElementById("resultpopupbox")
+    const resultpopupwindow = document.getElementById("resultpopupwindow")
+    const searchqueri = document.getElementById("hidden")
+    var loadingid
+    var called = false
+
+    //====checking all the radio button for change====
+
+    radiobutton.forEach(radio => {
+        radio.addEventListener("change", () => { engineswitcher(radio.value) })
+    })
+    //===============function to change the search engine according to the  radiobutton change=======
+
+    function engineswitcher(engine) {
+        logo.loading = "lazy"
+        switch (engine) {
+            case 'google':
+                logo.src = "img/google.png"
+                formdetails.action = "https://www.google.com/search"
+                user_question.name = "q"
+                user_question.placeholder = "Search through google..."
+                searchqueri.name = ""
+                searchqueri.value = ""
+                break
+
+            case 'wikipedia':
+                logo.src = "img/wikipedia.png"
+                formdetails.action = "https://en.wikipedia.org/w/index.php"
+                user_question.name = "search"
+                user_question.placeholder = "search through wikipedia page..."
+                searchqueri.name = "title"
+                searchqueri.value = "Special:Search"
+                break
+
+            case 'bing':
+                logo.src = "img/bing.png"
+                formdetails.action = "https://www.bing.com/search"
+                user_question.name = "q"
+                user_question.placeholder = "Search with Bing..."
+                searchqueri.name = ""
+                searchqueri.value = ""
+                break
+
+            case 'duckduckgo':
+                logo.src = "img/duckduckgo.png"
+                formdetails.action = "https://duckduckgo.com/"
+                user_question.name = "q"
+                user_question.placeholder = "Search with DuckDuckGo..."
+                searchqueri.name = ""
+                searchqueri.value = ""
+                break
+
+            default:
+                console.log("something might have gone wrong")
+                break;
+        }
+        console.log(engine)
+    }
+
+    //=============images to choose from===============
+
+    const images = [
+        'butterflies.webp',
+        'colors.jpg',
+        'driedtree.webp',
+        'headphone.jpg',
+        'hotairballon.webp',
+        'totoro.webp',
+        'water.webp',
+        'yourname.webp'
+    ]
+
+    //===========bg changer func=============
+
+    function backgroundImagechanger() {
+        const randomImage = images[Math.floor(Math.random() * images.length)]
+        document.body.style.backgroundImage = `url(img/${randomImage})`
+    }
+    //--------initial bg change-------
+    backgroundImagechanger()
+
+    //--------------changing on click----
+
+    changebg.addEventListener("click", () => { backgroundImagechanger(); })
+
+    //-------------checking if mouse is hovered and calling function
+
+    settingicon.addEventListener("mouseenter", () => showcontrollbox())
+
+    //-----to start the entry animation
+
+    function showcontrollbox() {
+        container.style.animation = "entry .5s ease-in forwards"
+        container.style.pointerEvents = "all"
+        if (!called) load()
+        container.addEventListener("animationstart", () => controllbox.style.zIndex = "3")
+    }
+
+    //-------------checking if mouse is hovered and calling function
+
+    container.addEventListener("mouseleave", () => hidecontrollbox())
+
+    //-----to start the leave animation
+
+    function hidecontrollbox() {
+        container.style.pointerEvents = "none"
+        container.style.animation = "leave .5s ease-in "
+        container.addEventListener("animationend", (event) => { if (event.animationName === 'leave') { controllbox.style.zIndex = "1" } })
+    }
+
+
+    //====function to load img to the container
+    
+    function load() {
+        called = true
+        images.forEach(img => {
+            var temp = document.createElement("img")
+            const i = document.createElement("img")
+            temp.src = `img/${img}`
+
+            const loadingid = setInterval(temp.addEventListener("load", () => {
+                i.loading = "lazy"
+                i.src = temp.src
+                bgimg.appendChild(i)
+            }), 1000)
+
+            clearInterval(loadingid)
+
+        })
+
+    }
+
+    //==end of dom ====
+})
+
